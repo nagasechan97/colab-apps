44f2fdfb4cb17c8363abf3f085082637fcb4caa7
nagasechan97 <67129230+nagasechan97@users.noreply.github.com>
2022-04-10 14:31:21 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -22,3 +22,70 @@ echo "$(tput setaf 4)                                                           
 echo "$(tput setaf 4) $(tput setaf 1) ❀ $(tput setaf 4)Subscribe To My Channel: https://bit.ly/3u4gpbQ"
 echo "$(tput setaf 4) $(tput setaf 1) ❀ $(tput setaf 4)Follow Me On Facebook: https://bit.ly/3LAPF8U"
 echo "$(tput setaf 6)=================================================================================================================================================================$(tput sgr 0)"
+echo "$(tput setaf 4) $(tput setaf 4) Checking And Installing Missing Dependencies..."
+echo "$(tput setaf 6)=================================================================================================================================================================$(tput sgr 0)"
+apt update
+apt install winbind cabextract
+clear
+echo "$(tput setaf 2)         █████████████████ $(tput sgr 0)"
+echo "$(tput setaf 2)        ██               ██ $(tput sgr 0)"
+echo "$(tput setaf 2)        ██               ██ $(tput sgr 0)"
+echo "$(tput setaf 2)        ██               ██ $(tput sgr 0)"
+echo "$(tput setaf 4)  ███████████████████████████████ $(tput sgr 0)"
+echo "$(tput setaf 4)██$(tput setaf 3)                              $(tput setaf 4) ██ $(tput sgr 0)"
+echo "$(tput setaf 4)██$(tput setaf 3)    //////////  //////////    $(tput setaf 4) ██ $(tput sgr 0)      $(tput setaf 3)  ____      _       _             $(tput setaf 6)    _                    "
+echo "$(tput setaf 4)██$(tput setaf 3) (((///////// ////////////((( $(tput setaf 4) ██ $(tput sgr 0)      $(tput setaf 3) / ___|___ | | __ _| |__         $(tput setaf 6)    / \   _ __  _ __  ___ "
+echo "$(tput setaf 4)██$(tput setaf 3)(((((        ////        (((((($(tput setaf 4)██ $(tput sgr 0)      $(tput setaf 3)| |   / _ \| |/ _  |  _ \ $(tput setaf 5)  _____ $(tput setaf 6)  / _ \ |  _ \|  _ \/ __| "
+echo "$(tput setaf 4)██$(tput setaf 3)((((        /////         ((((($(tput setaf 4)██ $(tput sgr 0)      $(tput setaf 3)| |__| (_) | | (_| | |_) |$(tput setaf 5) |_____| $(tput setaf 6)/ ___ \| |_) | |_) \__ \ "
+echo "$(tput setaf 4)██$(tput setaf 3)(((((        ////*       (((((($(tput setaf 4)██ $(tput sgr 0)      $(tput setaf 3) \____\___/|_|\__,_|_.__/     $(tput setaf 6)    /_/   \_\ .__/| .__/|___/"
+echo "$(tput setaf 4)██$(tput setaf 3) (((///////// //((((((((((((( $(tput setaf 4) ██ $(tput sgr 0)      $(tput setaf 6)                                          |_|   |_|        "
+echo "$(tput setaf 4)██$(tput setaf 3)    /////////.  ,(((((((((    $(tput setaf 4) ██ $(tput sgr 0)"
+echo "$(tput setaf 4)██$(tput setaf 3)                              $(tput setaf 4) ██ $(tput sgr 0)"
+echo "$(tput setaf 4) ██                             ██ $(tput sgr 0)"
+echo "$(tput setaf 4) ███                          ███ $(tput sgr 0)"
+echo "$(tput setaf 4)   ████████████████████████████ $(tput sgr 0)"
+echo ""
+echo "$(tput setaf 6)=================================================================================================================================================================$(tput sgr 0)"
+echo "$(tput setaf 4)                                                                      Made with $(tput setaf 1)❤ $(tput setaf 4)By Yui Sakura $(tput sgr 0)"                  
+echo "$(tput setaf 4) $(tput setaf 1) ❀ $(tput setaf 4)Subscribe To My Channel: https://bit.ly/3u4gpbQ"
+echo "$(tput setaf 4) $(tput setaf 1) ❀ $(tput setaf 4)Follow Me On Facebook: https://bit.ly/3LAPF8U"
+echo "$(tput setaf 6)=================================================================================================================================================================$(tput sgr 0)"
+echo "$(tput setaf 4) $(tput setaf 4) Hello q(≧▽≦q), What Do You Want To Do?
+MM='Please enter your choice: '
+options=("1. Install Software" "2. Install Desktop Environment" "3. Connect To Terminal Using SSH" "4. Mount Google Drive" "5. Suggest A New App" "6. Submit A New App" "7. Report An Error/Bug" "8. View Changelog" "9. Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Option 1")
+            echo "you chose choice 1"
+            ;;
+        "Option 2")
+            echo "you chose choice 2"
+            ;;
+        "Option 3")
+            echo "you chose choice $REPLY which is $opt"
+            ;;
+        "Option 4")
+            echo "you chose choice 1"
+            ;;
+        "Option 5")
+            echo "you chose choice 2"
+            ;;
+        "Option 6")
+            echo "you chose choice $REPLY which is $opt"
+            ;;
+        "Option 7")
+            echo "you chose choice 1"
+            ;;
+        "Option 8")
+            echo "you chose choice 2"
+            ;;
+        "Option 9")
+            echo "you chose choice $REPLY which is $opt"
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
