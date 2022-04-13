cf791cfcdb6863f225f71fee28667f149eb60649
nagasechan97 <67129230+nagasechan97@users.noreply.github.com>
2022-04-13 09:33:36 +0800

    Update AfterEffectsCS6.sh

--- Softwares/AdobeAfterEffectsCS6/AfterEffectsCS6.sh
+++ Softwares/AdobeAfterEffectsCS6/AfterEffectsCS6.sh
@@ -3,7 +3,7 @@
 echo "$(tput setaf 6)"
 echo "$========================================================================================================="
 echo "$(tput setaf 1) WARNING!!!"
-echo "$(tput setaf 6) After you start Adobe After Effects CS6 and the text "Initializing Media Core" appear, you need to wait for 15 second then close the After Effects CS6 Window from the taskbar. After that, the program will be stucked on "Initializing Scripting" close the After Effects CS6 Window from the taskbar again then the program will start."
+echo "$(tput setaf 6) After you start Adobe After Effects CS6 and the text \"Initializing Media Core\" appear, you need to wait for 15 second then close the After Effects CS6 Window from the taskbar. After that, the program will be stucked on \"Initializing Scripting\" close the After Effects CS6 Window from the taskbar again then the program will start."
 echo "$========================================================================================================="
 echo "Did you read the message?"
 PS3='Please enter your choice: '
